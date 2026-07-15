d18068a1e12efe3529319e8501e82dcf765689cf
Rajat Kumar <103815726+rajatkumar10@users.noreply.github.com>
2026-07-15 11:33:03 +0530

    Update: max-water-container - submission-1

--- Data Structures & Algorithms/max-water-container/submission-1.cpp
+++ Data Structures & Algorithms/max-water-container/submission-1.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
-    int maxArea(vector<int>& arr) {
-        int n=arr.size();
-        //sort(arr.begin(),arr.end());
-        int i=0,j=n-1;
-        int ans=0;
-        while(i<j){
-            int res=min(arr[i],arr[j])*(j-i);
-            ans=max(ans,res);
-            if(arr[i]<arr[j])i++;
-            else j--;
-        }
-        return ans;
+    void rotate(vector<int>& nums, int k) {
+        k=k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
